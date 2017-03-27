89d222db90b1f109fc439521b03ebe9221cf8fc2
Jeffrey Fu <jfucanada@users.noreply.github.com>
2017-03-26 18:25:14 -0700

    Create init.pp

---
+++ gssh/manifests/init.pp
@@ -0,0 +1,21 @@
+## Date:        2017-03-26
+## Owner:       Jeffrey Fu
+## Version:     puppet 3.8
+## Module:      gssh
+
+class gssh {
+
+        file {  '/root/.ssh/':
+                 ensure => 'directory',
+                 owner => 'root',
+                 group => 'root',
+                 mode => '0700',
+             }
+
+        file { '/root/.ssh/authorized_keys':
+                 content => template('gssh/authorized_keys_lnxprod.erb'),
+                 owner => 'root',
+                 group => 'root',
+                 mode => '0600',
+             }
+}
